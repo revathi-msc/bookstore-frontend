46530165d84d08e3eed9c4937ae3b6af319b5efb
Revathi Mahalingam <103559154+revathimsc@users.noreply.github.com>
2023-05-22 15:57:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import AddBook from "./components/AddBook";
 import Books from "./components/Book/Books";
-import About from "./components/About";
 import BookDetail from "./components/Book/BookDetail";
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
           <Route path="/" element={<Home />} exact />
           <Route path="/add" element={<AddBook />} exact />
           <Route path="/books" element={<Books />} exact />
-          <Route path="/about" element={<About />} exact />
           <Route path="/books/:id" element={<BookDetail />} exact />
         </Routes>
       </main>
